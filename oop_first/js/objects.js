4f17ddb38e6c5d5e0cbc98bf97a41e338f822b71
evgenii <kotlyarov.ev@gmail.com>
2017-11-30 23:20:46 +0600

    Играюсь с вызовом функции

--- oop_first/js/objects.js
+++ oop_first/js/objects.js
@@ -8,5 +8,26 @@ coffeeMachine.waterAmount = 200;
 
 var powerfulCoffeeMachine = new CoffeeMachine(1000);
 powerfulCoffeeMachine.waterAmount = 300;
- console.log(powerfulCoffeeMachine);
- console.log(coffeeMachine);
+
+
+
+function CoffeeMachine2(power) {
+    this.waterAmount = 0;
+
+    function getBoilTime() {
+        return 1000;
+    }
+
+    function onReady() {
+        alert('Кофе готов!');
+    }
+
+    this.run = function() {
+        setTimeout(onReady, getBoilTime());
+    };
+}
+
+var coffeeMachine2 = new CoffeeMachine2(100);
+coffeeMachine2.waterAmount = 200;
+
+coffeeMachine2.run();
