5183bf6cc70c797d345aefe8a8153604fb51937e
evgenii <kotlyarov.ev@gmail.com>
2017-12-01 00:47:52 +0600

    Использую наследование

--- oop_first/js/objects.js
+++ oop_first/js/objects.js
@@ -1,5 +1,19 @@
+function Machine() {
+    var enabled = false;
+
+    this.enable = function() {
+        enabled = true;
+    };
+
+    this.disable = function() {
+        enabled = false;
+    };
+}
+
+
 function CoffeeMachine(power) {
     this.waterAmount = 0;
+    Machine.call(this);
     console.log('Created coffee machine with: ' + power + ' watts' );
 }
 
@@ -13,6 +27,7 @@ powerfulCoffeeMachine.waterAmount = 300;
 
 function CoffeeMachine2(power) {
     this.waterAmount = 0;
+    Machine.call(this);
 
     function getBoilTime() {
         return 1000;
@@ -28,6 +43,20 @@ function CoffeeMachine2(power) {
 }
 
 var coffeeMachine2 = new CoffeeMachine2(100);
+coffeeMachine2.enable();
 coffeeMachine2.waterAmount = 200;
-
 coffeeMachine2.run();
+coffeeMachine2.disable();
+console.log(coffeeMachine.disable());
+
+var makeDrinkForPerson = function(drinkMachine, person) {
+    drinkMachine.waterAmount = 200;
+    var drink = drinkMachine.run();
+
+    person.give(drink);
+};
+var cocktailMaker = new CocktailMaker('rum', 'cola');
+var person = findPersonByName('John');
+
+makeDrinkForPerson(cocktailMaker, person);
+
