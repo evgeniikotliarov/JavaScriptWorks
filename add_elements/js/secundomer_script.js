0cd68e1e8972fad259611bb87894be72e85f2272
evgenii <kotlyarov.ev@gmail.com>
2017-12-10 23:47:13 +0600

    Сделал счетчик времени.

--- add_elements/js/secundomer_script.js
+++ add_elements/js/secundomer_script.js
@@ -8,4 +8,28 @@ $(function() {
     var interval;
     var secondsCounter = 0;
     var minutesCounter = 0;
+
+    start.on('click', function() {
+        if (!interval){
+            interval = setInterval(function () {
+                if (secondsCounter === 60){
+                    secondsCounter = 0;
+                    minutesCounter++;
+                    seconds.text(secondsCounter);
+                }
+                if(secondsCounter < 10){
+                    seconds.text('0' + secondsCounter)
+                }else{
+                    seconds.text(secondsCounter)
+                }
+                secondsCounter++;
+                if(minutesCounter < 10){
+                    minutes.text('0' + minutesCounter)
+                }else {
+                    minutes.text(minutesCounter)
+                }
+
+            },1000);
+        }
+    });
 });
