6161542fc9814b74cd223a0745dd5ad45feda914
evgenii <kotlyarov.ev@gmail.com>
2017-12-11 00:19:41 +0600

    Сделал проверку длинны введенных значений

---
+++ add_elements_and_validation_button/js/active_button_validator_script.js
@@ -0,0 +1,20 @@
+$(function () {
+    var userNameText;
+    var passwordText;
+
+    $('input[name="username"]').on('keyup', function(event) {
+        userNameText = event.target.value;
+
+        if (userNameText.length > 6 && passwordText.length > 6){
+            $('input[type="submit"]').attr('disabled', false);
+        }
+    });
+
+    $('input[name="password"]').on('keyup', function(event) {
+        passwordText = event.target.value;
+
+        if (passwordText.length > 6 && userNameText.length > 6){
+            $('input[type="submit"]').attr('disabled', false);
+        }
+    });
+});
