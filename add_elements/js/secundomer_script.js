51131fb0253c642c75b9df0961896d757e6f6824
evgenii <kotlyarov.ev@gmail.com>
2017-12-10 23:45:45 +0600

    Сделал разметку для секундомера

---
+++ add_elements/js/secundomer_script.js
@@ -0,0 +1,11 @@
+$(function() {
+    var seconds = $('#seconds');
+    var minutes = $('#minutes');
+
+    var start = $('#start');
+    var pause = $('#pause');
+    var reset = $('#reset');
+    var interval;
+    var secondsCounter = 0;
+    var minutesCounter = 0;
+});
