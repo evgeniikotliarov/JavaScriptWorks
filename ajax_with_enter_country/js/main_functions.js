b5061a7ea48d9ba8f2833211ee68fbbe1beaf0f0
evgenii <kotlyarov.ev@gmail.com>
2017-12-25 21:39:24 +0600

    Done example

--- ajax_with_enter_country/js/main_functions.js
+++ ajax_with_enter_country/js/main_functions.js
@@ -39,6 +39,28 @@ $(document).ajaxStop(function() {
     $('#preloader').hide();
 });
 
+function printCountry (response) {
+    for (var i = 0; i < response.length; i++ ) {
+        var newTableString = $('<tr>');
+        $('#table-body').append(newTableString);
+
+        var columnCode = $('<td>').html(response[i].alpha2Code);
+        var columnFlag = $('<td>');
+        var createImage = $('<img>').attr('src', response[i].flag).css({'width': '100px'});
+        columnFlag.append(createImage);
+
+        var columnName = $('<td>').html(response[i].name);
+        var columnCapital = $('<td>').html(response[i].capital);
+        var columnPopulation = $('<td>').html(response[i].population);
+
+        newTableString.append(columnCode);
+        newTableString.append(columnFlag);
+        newTableString.append(columnName);
+        newTableString.append(columnCapital);
+        newTableString.append(columnPopulation);
+    }
+}
+
 $('#show').on('click', function () {
     getInformation($('#country-name').val());
 });
