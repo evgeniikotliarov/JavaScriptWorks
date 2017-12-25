c9205aae701cb7e081e61d9a53ffac94da00f8b3
evgenii <kotlyarov.ev@gmail.com>
2017-12-25 22:02:17 +0600

    added translate

---
+++ translate_country_v/js/example.1.js
@@ -0,0 +1,28 @@
+
+
+let maps = [];
+
+let translate = {
+  name: 'Название',
+  topLevelDomain: 'Домен верхнего уровня',
+  alpha2Code: 'Альфа2код',
+  alpha3Code: 'Альфа3код',
+  callingCodes: 'Телефонный код',
+  capital: 'Столица',
+  altSpellings: 'Именование',
+  region: 'Регион',
+  subregion: 'Подрегион',
+  population: 'Население',
+  latlng: 'Широта, Долгота',
+  demonym: 'Этнохороним',
+  area: 'Площадь',
+  timezones: 'Часовые пояса',
+  borders: 'Граничит',
+  nativeName: 'Самоназвание',
+  numericCode: 'Трехзначный код страны',
+  currencies: 'Валюта',
+  languages: 'Языки',
+  flag: 'Флаг',
+  regionalBlocs: 'Региональные группы'
+};
+
