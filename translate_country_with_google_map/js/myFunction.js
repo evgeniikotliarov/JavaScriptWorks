5961d4f32417356ca33a7594549fe6f3fcc9380c
evgenii <kotlyarov.ev@gmail.com>
2018-01-07 23:50:07 +0600

    added request

---
+++ translate_country_with_google_map/js/myFunction.js
@@ -0,0 +1,21 @@
+var translate = {
+    name: 'Название',
+    borders: 'Пограничные страны'
+};
+function getInformation(name) {
+    $.ajax({
+        statusCode: {
+            404: function() {
+                alert( "page not found" );
+            }
+        },
+        method: 'GET',
+        url: "https://restcountries.eu/rest/v2/name/" + name,
+        success: printCountry
+    });
+
+}
+
+$('#show').on('click', function () {
+    getInformation($('#country-name').val());
+});
