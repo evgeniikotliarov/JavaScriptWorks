71271b27cd70ff9faa49e8dd761c98184855fdb8
evgenii <kotlyarov.ev@gmail.com>
2018-02-19 00:57:21 +0600

    React create todo

--- React/app.js
+++
@@ -1,7 +0,0 @@
-
-let element = React.createElement('h1', null, 'Hello React');
-let element2 = React.createElement('p', null, 'Library for created UI');
-const container = React.createElement('div', { className:'container'}, element, element2);
-
-
-ReactDOM.render(container, document.getElementById('root'));
