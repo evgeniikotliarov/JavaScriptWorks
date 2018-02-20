49fb6e9523dd1e122c7cfb641d48b42a3bb89f41
evgenii <kotlyarov.ev@gmail.com>
2018-02-21 01:28:51 +0600

    Сделал декомпозицию компонентов

--- React/todo/src/App.js
+++ React/todo/src/App.js
@@ -1,37 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Todo from "./components/Todo";
+import './components/Styles.css'
 
 class App extends Component {
     props;
   render() {
     return (
       <main>
-        <header>
-          <h1>{this.props.title}</h1>
-        </header>
-        <section className="todo-list">
-            <div className="todo completed">
-                <button className="checkbox icon">
-                    <i className="material-icons">check_box</i>
-                </button>
-                <button className="delete icon">
-                    <i className="material-icons">delete</i>
-                </button>
-                <span className="todo-title">
-                    Learn JS
-                </span>
-            </div>
-            <div className="todo">
-                <button className="checkbox icon">
-                    <i className="material-icons">check_box_outline_blank</i>
-                </button>
-                <button className="delete icon">
-                    <i className="material-icons">delete</i>
-                </button>
-                <span className="todo-title">
-                    Learn React
-                </span>
-            </div>
+          <Header title={this.props.title}/>
+        <section className="todo">
+            <Todo title={"Learn Es6"} completed={true}/>
+            <Todo title={"Learn React*"} completed={false}/>
         </section>
       </main>
     );
--- React/todo/src/index.js
+++ React/todo/src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+
 ReactDOM.render(<App title="React Todo" />, document.getElementById('root'));
 registerServiceWorker();
