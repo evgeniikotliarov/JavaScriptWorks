b56cb2a86cfeb9051207c6fcd8b0f3e239471334
evgenii <kotlyarov.ev@gmail.com>
2018-02-25 20:44:08 +0600

    Added form for editing

--- React/todo/src/components/Todo.js
+++ React/todo/src/components/Todo.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Checkbox from "./Checkbox";
 import Button from "./Button";
 import './Styles.css';
 
-function Todo(props) {
-    return (
-        <div className={`todo${props.completed ? " completed" : ''}`}>
-            <Checkbox checked={props.completed} onChange={() => props.onStatusChange(props.id)} />
-            <span className="todo-title">{props.title}</span>
-            <Button className="delete icon" icon="delete" onClick={() => props.onDelete(props.id)} />
-        </div>
-    );
+
+class Todo extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            editing: true
+        };
+    }
+    render() {
+        return(this.state.editing ?
+            <form className="todo-edit-form" onSubmit={this.handleSubmit}>
+                <input type="text" ref="title" defaultValue={this.props.title}/>
+                <Button className="save icon" icon="save" type="submit"/>
+            </form>
+            :
+            <div className={`todo${this.props.completed ? " completed" : ''}`}>
+                <Checkbox checked={this.props.completed} onChange={() => this.props.onStatusChange(this.props.id)} />
+                <span className="todo-title">{this.props.title}</span>
+                <Button className="edit icon" icon="edit"/>
+                <Button className="delete icon" icon="delete" onClick={() => this.props.onDelete(this.props.id)} />
+            </div>
+        );
+    }
 }
+
 Todo.propTypes = {
     title: PropTypes.string.isRequired,
     completed: PropTypes.bool.isRequired,
