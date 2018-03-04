c4fb277a845bd87ce7228499492fc20dec913e5c
evgenii <kotlyarov.ev@gmail.com>
2018-03-05 01:23:54 +0600

    Added animation with css transition

--- React/personal-ui/src/App.js
+++ React/personal-ui/src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from "./components/Form";
+import {CSSTransitionGroup} from "react-transition-group";
 
 class App extends Component {
+
+  state = {
+    mounted: true
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ mounted: false})
+  };
+
   render() {
     return (
       <div className="App">
         <div className="my-form">
-          <Form/>
+          <CSSTransitionGroup
+            transitionNmae="fade"
+            transitionAppear={true}
+            transitionAppearTimeout={1500}
+            transitionEnter={false}
+            transitionLeave={true}
+            transitionLeaveTimeout={1500}>
+              {this.state.mounted && <Form onSubmit={this.handleSubmit}/>}
+          </CSSTransitionGroup>
         </div>
 
       </div>
--- React/personal-ui/src/components/Form.js
+++ React/personal-ui/src/components/Form.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Input from "./Input";
 
-function Form() {
+function Form({onSubmit}) {
     return(
-        <form className="sig-nup-form mdc-theme--dark">
+        <form className="sig-nup-form mdc-theme--dark" onSubmit={onSubmit}>
             <Input type="text" name="firstname" placeholder="Имя"/>
             <Input type="text" name="lastname" placeholder="Фамилия"/>
             <Input type="email" name="email" placeholder="Email"/>
             <Input type="password" name="password" placeholder="Пароль"/>
-            <button className="mdc-button mdc-button--stroked mdc-button--raised">Зарегистрироваться</button>
+            <button type="submit" className="mdc-button mdc-button--stroked mdc-button--raised">Зарегистрироваться</button>
         </form>
     );
 }
