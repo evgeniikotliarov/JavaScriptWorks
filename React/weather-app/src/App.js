ab3cb98149af910c60765431735bc673b308bd68
evgenii <kotlyarov.ev@gmail.com>
2018-03-13 00:41:58 +0600

    weather-app

--- React/weather-app/src/App.js
+++ React/weather-app/src/App.js
@@ -1,20 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import WeatherIcon from "./components/WeatherIcon";
+import WeatherDetails from "./components/WeatherDetails";
 
 class App extends Component {
+    state = {
+      icon: '',
+      time: 1,
+      city: '',
+      temperature: '',
+      weatherCode: '',
+      fetching: true
+    };
+
+    componentDidMount() {
+      this.fetchIp();
+    }
+
+    fetchWeatherDate = city => {
+      const baseUrl = `http://openweathermap.org/`;
+      const path = `data/2.5/forecast`;
+      const appId = `0d1080383ad1fd52cb624cb6722ea8fc`;
+      const query = `units=metric&lang=ru&APPID=${appId}`;
+
+      fetch(`${baseUrl}${path}?id=${city}&${query}`)
+        .then(response => response.json())
+        .then(data => {
+          const date = new Date();
+          const time = date.getHours();
+          console.log(response);
+
+          this.setState({
+            time,
+            city,
+            temperature: Math.round(data.main.temp),
+            weatherCode: data.weather[0].id,
+            fetching: false
+          });
+        })
+        .catch(error => console.error(error));
+    };
+
+    fetchIp = () => {
+      fetch('//freegeoip.net/json/')
+        .then(response => response.json())
+        .then(({city}) => this.fetchWeatherDate(city))
+        .catch(error => console.log(error));
+    };
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    const { fetching, icon, time, city, temperature, weatherCode } = this.state;
+    return fetching ?
+      <div className="App">Loading...</div>
+      :
+      <div className="App" data-hour={time}>
+        <WeatherIcon
+          icon={icon}
+          weatherCode={weatherCode}
+          time={time} />
+        <WeatherDetails
+          city={city}
+          temperature={temperature}
+        />
+      </div>;
   }
 }
 
